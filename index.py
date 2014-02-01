f5312a0e303868adfb1f2b8cd993f99624d980ca
Jisha <thejishajacob@gmail.com>
2014-02-01 13:07:09 -0500

    disabling news function

--- index.py
+++ index.py
@@ -4,14 +4,14 @@
 from datetime import datetime
 from os import system
 import pywapi
-import feedparser
+#import feedparser
 
 #Jarvis Specific Libraries
 from greeting import greeting
 from weather import weather
 from coffee import coffee
 from dinner import dinner
-from current_events import current_events, bbc_podcast
+#from current_events import current_events, bbc_podcast
 
 
 now = datetime.now()
@@ -23,13 +23,13 @@ def main():
     if period == 'AM':
         greeting(now);
         weather(weather_result);
-        current_events(now);
+        #current_events(now);
         coffee(now);
-        bbc_podcast(now);
+        #bbc_podcast(now);
     else:
         greeting(now);
         weather(weather_result);
-        current_events(now);
+        #current_events(now);
         dinner(now);
 
 if __name__ == "__main__":
